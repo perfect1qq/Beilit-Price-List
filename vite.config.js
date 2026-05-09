8128607bd3228a912e214f3ac56ed00735e51726
perfect1qq <101235247+perfect1qq@users.noreply.github.com>
2026-05-09 09:30:47 +0800

    修复bug

--- vite.config.js
+++ vite.config.js
@@ -40,8 +40,8 @@ export default defineConfig({
         short_name: 'Beilit',
         theme_color: '#2563eb',
         icons: [
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+          { src: 'logo.png', sizes: '192x192', type: 'image/png' },
+          { src: 'logo.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     }),
