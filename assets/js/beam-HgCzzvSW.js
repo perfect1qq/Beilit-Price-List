0d23b0eb6dc3acf063207fe4514ac971cb7e1bfe
perfect1qq <101235247+perfect1qq@users.noreply.github.com>
2026-06-11 23:26:20 +0000

    Deploying to gh-pages from @ perfect1qq/Beilit-Price-List@92014970f88329ea63b736b4a57e26a8f7246b8a 🚀

---
+++ assets/js/beam-HgCzzvSW.js
@@ -0,0 +1,1 @@
+import{b as a,s as t}from"./index-B5WmWh0o.js";const s=e=>t.get("/api/beam-quotations",{params:e}),c=e=>t.post("/api/beam-quotations",e),i=(e,o)=>t.put(`/api/beam-quotations/${e}`,o),m=e=>t.delete(`/api/beam-quotations/${e}`),p=e=>t.get("/api/beam-quotations/check-name",{params:{name:e}}),r={list:a(s),checkName:a(p),create:a(c),update:a(i),remove:a(m)};export{r as b};
