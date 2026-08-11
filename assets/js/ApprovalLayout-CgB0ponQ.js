fc8cc5c99645db2b0c26fbc68ffb5060a41b995b
perfect1qq <101235247+perfect1qq@users.noreply.github.com>
2026-08-11 15:39:01 +0000

    Deploying to gh-pages from @ perfect1qq/Beilit-Price-List@d58f69ec5920da8ec75191451da9fd758081d7d6 🚀

---
+++ assets/js/ApprovalLayout-CgB0ponQ.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-FNSJ_0c8.js";import{J as t,K as e,at as c}from"./vendor-vue-DbwCLwoH.js";import"./vendor-element-plus-lSjLdHzv.js";import"./vendor-misc-CSPwPqeS.js";import"./vendor-element-icons-DynAIvYn.js";import"./vendor-axios-42ANG6Sg.js";const n={};function p(a,s){const o=c("router-view");return t(),e(o)}const v=r(n,[["render",p]]);export{v as default};
